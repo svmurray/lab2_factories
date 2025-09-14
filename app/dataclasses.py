2f84ead09283c9b4e0dfdee1dedaacf02fdf278d
svmurray <38384015+svmurray@users.noreply.github.com>
2025-09-14 17:27:44 -0500

    Naive_v1

--- app/dataclasses.py
+++ app/dataclasses.py
@@ -5,3 +5,17 @@ class Email:
     """Dataclass representing an email with subject and body"""
     subject: str
     body: str
+
+@dataclass
+class EmailWithTopic:
+    """Dataclass representing an email with subject, body, and topic"""
+    subject: str
+    body: str
+    topic: str
+
+@dataclass
+class Topic:
+"""Dataclass representing an email with topic and description"""
+    topic: str
+    description: str
+    
